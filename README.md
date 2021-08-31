4cab24d4dd31484dcbc691399dc0fc9af6da9e27
Anthony Garcia <Anthony.sg20@gmail.com>
2021-08-31 10:50:39 -0700

    Added 2nd entry from local machine

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ first repo
 
 ## add 1st entry
 guac and chips sounds good
+
+## add 2nd enrty from local machine
+I l'll bring the brews
---
+++ guac_mix.java
@@ -0,0 +1,10 @@
+// Patry Message
+
+public class Guac
+{
+  public static void public static void main(String[] args) {
+
+    System.out.println("Lets make Guacamole!");
+
+  }
+}
